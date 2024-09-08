81a2382500603da4441a25bc0b0e43e3e28f28d2
Sumeya Muhammed <sumenaweya@gmail.com>
2024-09-08 09:53:39 -1100

    in/decrease function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,3 +9,34 @@ products.forEach((product) => {
     img.classList.add("selected");
   });
 });
+
+const quantity_box = document.querySelectorAll(".products .quantity");
+quantity_box.forEach((quantity_box) => {
+  const decrease = quantity_box.querySelector(".decrease");
+  decrease.addEventListener("click", () => {
+    var quantity = quantity_box.querySelector("span");
+    var number = quantity.innerHTML;
+    console.log(number);
+    num = parseInt(number, 10);
+    console.log(num);
+    if (num > 0) {
+      num = num - 1;
+      console.log(num);
+      quantity.innerHTML = num;
+    } else {
+      return;
+    }
+  });
+
+  const increase = quantity_box.querySelector(".increase");
+  increase.addEventListener("click", () => {
+    var quantity = quantity_box.querySelector("span");
+    var number = quantity.innerHTML;
+    console.log(number);
+    num = parseInt(number, 10);
+    console.log(num);
+    num += 1;
+    console.log(num);
+    quantity.innerHTML = num;
+  });
+});
