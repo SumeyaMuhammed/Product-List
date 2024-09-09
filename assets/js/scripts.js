9651f4e749416f18b02308f8719f3236f7eb70ce
Sumeya Muhammed <sumenaweya@gmail.com>
2024-09-08 23:45:14 -1100

    simplified add cart and quantity functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,58 +1,4 @@
 const products = document.querySelectorAll(".products");
-products.forEach((product) => {
-  const added_cart = document.querySelector(".added-cart");
-  const empty_cart = document.querySelector(".empty-cart");
-  result = 0;
-
-  const add_to_cart = product.querySelector(".add-cart");
-  const quantity = product.querySelector(".quantity");
-  const img = product.querySelector("img");
-  add_to_cart.addEventListener("click", () => {
-    add_to_cart.style.display = "none";
-    quantity.style.display = "flex";
-    img.classList.add("selected");
-
-    const decrease = product.querySelector(".decrease");
-    decrease.addEventListener("click", () => {
-      var quantity = product.querySelector(".quantity span");
-      var number = quantity.innerHTML;
-      console.log(number);
-      num = parseInt(number, 10);
-      if (num > 0) {
-        num = num - 1;
-        quantity.innerHTML = num;
-        result -= 1;
-        total_item.innerHTML = result;
-      } else {
-        return;
-      }
-      if (result == 0) {
-        empty_cart.style.display = "block";
-        added_cart.style.display = "none";
-      }
-    });
-
-    const increase = product.querySelector(".increase");
-    increase.addEventListener("click", () => {
-      var quantity = product.querySelector(".quantity span");
-      console.log(quantity);
-      var number = quantity.innerHTML;
-      console.log(number);
-      num = parseInt(number, 10);
-      num += 1;
-      quantity.innerHTML = num;
-      console.log(quantity.innerHTML);
-      result += 1;
-      total_item.innerHTML = result;
-      empty_cart.style.display = "none";
-      added_cart.style.display = "block";
-    });
-  });
-});
-
-const quantity_box = document.querySelectorAll(".products .quantity");
-var total_item = document.querySelector(".ordered-cart h2 span");
-
 const added_list = document.querySelectorAll("ul");
 added_list.forEach((list) => {
   const item_name = list.querySelector("h4");
@@ -60,5 +6,56 @@ added_list.forEach((list) => {
   const item_price = list.querySelector(".item-price");
   const total_price = list.querySelector(".total-price");
 });
+products.forEach((product) => {
+  const added_cart = document.querySelector(".added-cart");
+  const empty_cart = document.querySelector(".empty-cart");
+  var total_item = document.querySelector(".ordered-cart h2 span");
+  result = 0;
+  const quantity = product.querySelector(".quantity");
+  const img = product.querySelector("img");
+  const list_name = product.querySelector(".caption h3");
+  const list_price = product.querySelector(".caption p");
 
-quantity_box.forEach((quantity_box) => {});
+  const add_to_cart = product.querySelector(".add-cart");
+  add_to_cart.addEventListener("click", () => {
+    add_to_cart.style.display = "none";
+    quantity.style.display = "flex";
+    img.classList.add("selected");
+  });
+
+  const decrease = product.querySelector(".decrease");
+  decrease.addEventListener("click", () => {
+    var quantity = product.querySelector(".quantity span");
+    var number = quantity.innerHTML;
+    console.log(number);
+    num = parseInt(number, 10);
+    if (num > 0) {
+      num = num - 1;
+      quantity.innerHTML = num;
+      result -= 1;
+      total_item.innerHTML = result;
+    } else {
+      return;
+    }
+    if (result == 0) {
+      empty_cart.style.display = "block";
+      added_cart.style.display = "none";
+    }
+  });
+
+  const increase = product.querySelector(".increase");
+  increase.addEventListener("click", () => {
+    var quantity = product.querySelector(".quantity span");
+    console.log(quantity);
+    var number = quantity.innerHTML;
+    console.log(number);
+    num = parseInt(number, 10);
+    num += 1;
+    quantity.innerHTML = num;
+    console.log(quantity.innerHTML);
+    result += 1;
+    total_item.innerHTML = result;
+    empty_cart.style.display = "none";
+    added_cart.style.display = "block";
+  });
+});
