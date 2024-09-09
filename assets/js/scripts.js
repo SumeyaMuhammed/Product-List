bd4a194f6ca45389b6ab188a146e4a7f4d17ca2a
Sumeya Muhammed <sumenaweya@gmail.com>
2024-09-08 23:20:54 -1100

    edited add to cart

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,9 @@
 const products = document.querySelectorAll(".products");
 products.forEach((product) => {
+  const added_cart = document.querySelector(".added-cart");
+  const empty_cart = document.querySelector(".empty-cart");
+  result = 0;
+
   const add_to_cart = product.querySelector(".add-cart");
   const quantity = product.querySelector(".quantity");
   const img = product.querySelector("img");
@@ -7,49 +11,54 @@ products.forEach((product) => {
     add_to_cart.style.display = "none";
     quantity.style.display = "flex";
     img.classList.add("selected");
+
+    const decrease = product.querySelector(".decrease");
+    decrease.addEventListener("click", () => {
+      var quantity = product.querySelector(".quantity span");
+      var number = quantity.innerHTML;
+      console.log(number);
+      num = parseInt(number, 10);
+      if (num > 0) {
+        num = num - 1;
+        quantity.innerHTML = num;
+        result -= 1;
+        total_item.innerHTML = result;
+      } else {
+        return;
+      }
+      if (result == 0) {
+        empty_cart.style.display = "block";
+        added_cart.style.display = "none";
+      }
+    });
+
+    const increase = product.querySelector(".increase");
+    increase.addEventListener("click", () => {
+      var quantity = product.querySelector(".quantity span");
+      console.log(quantity);
+      var number = quantity.innerHTML;
+      console.log(number);
+      num = parseInt(number, 10);
+      num += 1;
+      quantity.innerHTML = num;
+      console.log(quantity.innerHTML);
+      result += 1;
+      total_item.innerHTML = result;
+      empty_cart.style.display = "none";
+      added_cart.style.display = "block";
+    });
   });
 });
 
 const quantity_box = document.querySelectorAll(".products .quantity");
 var total_item = document.querySelector(".ordered-cart h2 span");
-console.log(total_item);
-// var total_item_number = parseInt(total_item, 10);
-// console.log(total_item_number);
-result = 0;
 
-quantity_box.forEach((quantity_box) => {
-  const decrease = quantity_box.querySelector(".decrease");
-  decrease.addEventListener("click", () => {
-    var quantity = quantity_box.querySelector("span");
-    // console.log(quantity);
-    var number = quantity.innerHTML;
-    // console.log(number);
-    num = parseInt(number, 10);
-    console.log(num);
-    if (num > 0) {
-      num = num - 1;
-      // console.log(num);
-      quantity.innerHTML = num;
-      result -= 1;
-      console.log(result);
-      total_item.innerHTML = result;
-    } else {
-      return;
-    }
-  });
-
-  const increase = quantity_box.querySelector(".increase");
-  increase.addEventListener("click", () => {
-    var quantity = quantity_box.querySelector("span");
-    var number = quantity.innerHTML;
-    // console.log(number);
-    num = parseInt(number, 10);
-    // console.log(num);
-    num += 1;
-    // console.log(num);
-    quantity.innerHTML = num;
-    result += 1;
-    console.log(result);
-    total_item.innerHTML = result;
-  });
+const added_list = document.querySelectorAll("ul");
+added_list.forEach((list) => {
+  const item_name = list.querySelector("h4");
+  const item_count = list.querySelector(".item-count");
+  const item_price = list.querySelector(".item-price");
+  const total_price = list.querySelector(".total-price");
 });
+
+quantity_box.forEach((quantity_box) => {});
